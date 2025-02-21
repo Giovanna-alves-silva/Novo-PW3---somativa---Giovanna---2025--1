99731759ed8f4611dc172d3e09ce9133a475d28b
Giovanna-alves-silva <giovanna.alves.silva.sp@gmail.com>
2025-02-20 22:29:13 -0300

    inicio da barra de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import './App.css'
-
-import BookCard from './components/BookCard'
-
-import capa_livro from './assets/cavernas_aco.jpg'
+import NavBar from './components/layout/NavBar'
 
 function App() {
 
   return (
     <>
       <div>
-
-          <h1>PW3 - WEBAPP - LIVRARIA</h1>
-
-          <BookCard
-            titulo='Carvenas de aço' 
-            autor= 'Isaac Asmov'
-            imagem= {capa_livro}/> 
-
-          {/* <BookCard
-            titulo='O quarto de despejo' 
-            autor= 'Maria Carolina de Jesus'
-            imagem= 'IMAGEM'/>
-
-          <BookCard
-            titulo='Iracema' 
-            autor= 'José de Alencar'
-            imagem= 'IMAGEM'/>
-
-          <BookCard
-            titulo='O Guarani' 
-            autor= 'José de Alencar'
-            imagem= 'IMAGEM'/> */}
-
-
+        <NavBar />
       </div>
     </>
   )
