91817cca2f73161b42affd45b9a60e981e078bba
Giovanna-alves-silva <giovanna.alves.silva.sp@gmail.com>
2025-03-05 17:07:17 -0300

    telas de navegação(listBook e CreateBook), instalação do router Dom e adição de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
+
+import Container from './components/layout/container'
+
+import Home from './components/pages/Home'
+import CreateBook from './components/pages/CreateBook'
+import ListBook from './components/pages/ListBook'
+
 import NavBar from './components/layout/NavBar'
 
 function App() {
-
+ 
   return (
     <>
       <div>
-        <NavBar />
+        
+        <BrowserRouter>
+        <Container>
+          <Routes>
+            <Route path = '/' element={<NavBar />}>
+
+            <Route path='/' element={<Home />} />
+            <Route path='/newBook' element={<CreateBook />} />
+            <Route path='/listBook' element={<ListBook />} />
+
+            </Route>
+          </Routes>
+        
+        </Container>
+
+        </BrowserRouter>
+ 
       </div>
     </>
   )
 }
-
+ 
 export default App
