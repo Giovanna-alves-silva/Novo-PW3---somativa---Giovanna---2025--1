6151cb92592d402fb7f8e2b1530cfdfbf36f2434
Giovanna-alves-silva <giovanna.alves.silva.sp@gmail.com>
2025-05-15 20:14:57 -0300

    Botão detalhe, editar e excluir. Funcionalidade do botão detalhe do livro

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Container from './components/layout/Container'
 import Home from './components/pages/Home'
 import CreateBook from './components/pages/CreateBook'
 import ListBook from './components/pages/ListBook'
+import DetailBook from './components/pages/DetailBook'
+
 import NavBar from './components/layout/NavBar'
 
 function App() {
@@ -26,6 +28,7 @@ function App() {
                 <Route path='/' element={<Home />} />
                 <Route path='/newBook' element={<CreateBook />} />
                 <Route path='/listBook' element={<ListBook />} />
+                <Route path='/detailBook/:cod_livro' element={<DetailBook />} />
 
               </Route>
 
